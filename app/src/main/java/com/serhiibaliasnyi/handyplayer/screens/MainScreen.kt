ac49d01d2e07b559cdb73be4b0306d02667258bd
Baliasnyi Serghii <sergey070373@gmail.com>
2024-03-24 23:23:39 +0000

    button

--- app/src/main/java/com/serhiibaliasnyi/handyplayer/screens/MainScreen.kt
+++ app/src/main/java/com/serhiibaliasnyi/handyplayer/screens/MainScreen.kt
@@ -1,33 +1,50 @@
 package com.serhiibaliasnyi.handyplayer.screens
 
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -57,16 +74,16 @@ fun MainScreen(){
                   .constrainAs(firstRect) {
                       //centerTo(parent)
                       start.linkTo(parent.start, margin = 0.dp)
-                     // end.linkTo(parent.end, margin = 0.dp)
+                      // end.linkTo(parent.end, margin = 0.dp)
                       top.linkTo(parent.top, margin = 0.dp)
                       //bottom.linkTo(parent.bottom, margin = 0.dp)
-                      width = Dimension.percent(0.5f)
+                      width = Dimension.percent(0.6f)
                       //height=Dimension.value(300.dp)
                       //height = Dimension.fillToConstraints
                       height = Dimension.percent(0.7f)
                   }
                   //.background(color = Color.Red)
-                  .background(color = Color(125,150,141))
+                  .background(color = Color(125, 150, 141))
 
               ) {
                   Image(
@@ -87,18 +104,39 @@ fun MainScreen(){
                       // end.linkTo(parent.end, margin = 0.dp)
                       top.linkTo(firstRect.bottom, margin = 0.dp)
                       bottom.linkTo(parent.bottom, margin = 0.dp)
-                      width = Dimension.percent(0.5f)
+                      width = Dimension.percent(0.6f)
                       //height=Dimension.value(300.dp)
                       //height = Dimension.fillToConstraints
                       height = Dimension.percent(0.3f)
                   }
-                  .background(color = Color(0,81,65))
+                  .background(color = Color(0, 81, 65))
               ) {
+
+
+
+
+                  Button(onClick = { },
+
+                     // modifier= Modifier.size(100.dp),
+                      shape = CircleShape,
+                     // border= BorderStroke(5.dp, Color(0XFF0F9D58)),
+                      contentPadding = PaddingValues(0.dp),
+                      //colors = ButtonDefaults.outlinedButtonColors(contentColor =  Color.Blue)
+                      colors = ButtonDefaults.outlinedButtonColors()
+                      //colors = ButtonDefaults.Transparent
+                  ) {
+                      // Adding an Icon "Add" inside the Button
+                      Icon(imageVector =ImageVector.vectorResource(R.drawable.button_play) ,
+                          contentDescription = "content description",
+                          tint=Color(246,151,64),
+                          modifier = Modifier.size(100.dp)
+                      )
+                  }
 
               }
 
               Box(modifier =
-                  Modifier
+              Modifier
                   .constrainAs(createRef()) {
                       // centerTo(parent)
                       start.linkTo(firstRect.end, margin = 10.dp)
@@ -109,7 +147,7 @@ fun MainScreen(){
                       height = Dimension.fillToConstraints
                       //  height=Dimension.value(300.dp)
                   }
-                  .background(color = Color(125,150,141))
+                  .background(color = Color(125, 150, 141))
               ) {
                    LazyColumn {
                     itemsIndexed(list){index, title->
@@ -123,8 +161,9 @@ fun MainScreen(){
                             .padding(3.dp)
                           ) {
                         Text(text = title,
+                            textAlign = TextAlign.Center,
                             fontFamily = FontFamily.Serif,
-                            fontSize = 20.sp,
+                            fontSize = 22.sp,
                             fontWeight = FontWeight.Bold,
                             color = Color.White,
                             modifier = Modifier
@@ -132,8 +171,9 @@ fun MainScreen(){
                                 .clickable {
 
                                 }
-                                .padding(0.dp,20.dp)
+                                .padding(0.dp, 20.dp)
                                 .wrapContentWidth()
+
                         )
                     }
                 }
